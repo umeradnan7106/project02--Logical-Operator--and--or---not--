04d286694db802371c579e225e6ea1f8742026ad
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2024-07-10 03:54:08 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,40 @@
+            //  Logical Operator
+  
+let Budget = 600000;
+let haveGirl = true;
+
+          //    false         &&        true      =         False  
+console.log(Budget >= 1000000 && haveGirl == true);
+
+         //       true       &&         true      =         true  
+console.log(Budget >= 600000 && haveGirl == true);
+
+
+
+             // && Operator
+
+let budget = 600000;
+let HaveGirl = true;
+let result =  budget >= 600000 && HaveGirl == true;
+
+console.log(result);
+
+
+
+             // || Operator
+
+let haveDegree = "yes";
+let isHamzasStd = "no"
+
+             //       true       ||            false     =       true
+let answer = haveDegree == "yes" || isHamzasStd == "yes";
+console.log(answer)
+
+
+
+             // ! Operator
+
+let num1 = 10;
+let output = !(num1 < 10);
+
+console.log(output);
